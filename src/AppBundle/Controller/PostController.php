595308b657a4cc496f88e32bfc5e9c1e06d0cdf4
8785496 <georg.88@mail.ru>
2015-06-28 20:39:49 +0600

    Аутентификация, авторизация, создание, редактирование, удаление постов  Changes to be committed: 	modified:   app/Resources/views/base.html.twig 	deleted:    app/Resources/views/default/index.html.twig 	deleted:    app/Resources/views/post/index.html.twig 	new file:   app/Resources/views/post/posts.html.twig 	new file:   app/Resources/views/security/login.html.twig 	modified:   app/config/routing.yml 	modified:   app/config/security.yml 	deleted:    src/AppBundle/Controller/DefaultController.php 	deleted:    src/AppBundle/Controller/HelloController.php 	modified:   src/AppBundle/Controller/PostController.php 	new file:   src/AppBundle/Controller/SecurityController.php 	deleted:    src/AppBundle/Entity/Product.php

--- src/AppBundle/Controller/PostController.php
+++ src/AppBundle/Controller/PostController.php
@@ -55,5 +55,67 @@ class PostController extends Controller
             'form' => $form->createView(),
         ));
     }
+    /**
+     * @Route("/update/{id}", name="post_update")
+     */
+    public function updateAction($id, Request $request) 
+    {
+        $em = $this->getDoctrine()->getManager();
+        $post = $em->getRepository('AppBundle:Post')->find($id);
+
+        if (!$post) {
+            throw $this->createNotFoundException(
+                'No post found for id '.$id
+            );
+        }
+        
+        $form = $this->createFormBuilder($post)
+            ->add('authorId', 'hidden')    
+            ->add('content', 'text')
+            ->add('refresh', 'submit', array('label' => 'Refresh Post'))
+            ->getForm();
+        
+        $form->handleRequest($request);
+        
+        if ($form->isValid()) {
+            $em->persist($post);
+            $em->flush();
+            return $this->redirectToRoute('post_id', ['id' => $post->getPostId()]);
+        }
+
+        return $this->render('post/create.html.twig', ['form' => $form->createView()]);
+    }
+    /**
+     * @Route("/", name="home")
+     */
+    public function postsAction()
+    {
+        $posts = $this->getDoctrine()
+            ->getRepository('AppBundle:Post')
+            ->findAll();
+        
+        return $this->render('post/posts.html.twig', [
+            'posts' => $posts
+        ]);
+    }
+    /**
+     * @Route("/delete/{id}", name="post_delete")
+     */
+    public function deleteAction($id) 
+    {
+        $em = $this->getDoctrine()->getManager();
+        $post = $em->getRepository('AppBundle:Post')->find($id);
+        
+        if (!$post) {
+            throw $this->createNotFoundException(
+                'No post found for id '.$id
+            );
+        }
+        
+        $em->remove($post);
+        $em->flush();
+        
+        return $this->redirectToRoute('home');
+    }
 }
 
